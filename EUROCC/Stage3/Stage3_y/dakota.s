eb13c771f9f96331e9b34825f0bf03e66545e69f
Mustafa Kuzay <90314532+mkuzaay@users.noreply.github.com>
2021-09-15 11:34:07 +0300

    Update dakota.s

--- EUROCC/Stage3/Stage3_y/dakota.s
+++ EUROCC/Stage3/Stage3_y/dakota.s
@@ -8,7 +8,7 @@
 #SBATCH --workdir=/truba/home/mkuzay/CfdSim/SlotOpt/model_3_1_kopya
 #SBATCH --output=/truba/home/mkuzay/CfdSim/SlotOpt/model_3_1_kopya/slurm-%j.out
 #SBATCH --error=/truba/home/mkuzay/CfdSim/SlotOpt/model_3_1_kopya/slurm-%j.err
-#SBATCH --mail-user=mkuzaay3@gmail.com
+#SBATCH --mail-user=
 #SBATCH --mail-type=ALL
 
 source $HOME/OpenFOAM/OpenFOAM-4.1/etc/bashrc WM_LABEL_SIZE=64 WM_COMPILER_TYPE=ThirdParty FOAMY_HEX_MESH=yes
